48ea269be4938b3cf1a8755e1591092843151b4f
GGGuang12138 <ggguang12138@icloud.com>
2024-03-14 22:50:23 +0800

    合并

---
+++ src/main/java/leetcode/permutations/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.permutations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alan
+ * @Description
+ * @date 2024.03.12 20:27
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String input = "ABCDE";
+        List<String> permutations = generatePermutations(input);
+        for (String permutation : permutations) {
+            System.out.println(permutation);
+        }
+    }
+
+    // 生成给定字符串的所有排列
+    public static List<String> generatePermutations(String input) {
+        List<String> permutations = new ArrayList<>();
+        generatePermutationsHelper("", input, permutations);
+        return permutations;
+    }
+
+    // 递归辅助方法，生成给定前缀和剩余字符的所有排列
+    private static void generatePermutationsHelper(String prefix, String remaining, List<String> permutations) {
+        int n = remaining.length();
+        // 如果剩余字符为空，则将当前排列添加到结果列表
+        if (n == 0) {
+            permutations.add(prefix);
+        } else {
+            // 否则，对于剩余字符中的每一个字符，递归生成排列
+            for (int i = 0; i < n; i++) {
+                // 递归调用，将当前字符添加到前缀，剩余字符更新为不包含当前字符的子串
+                String remaining1 = remaining.substring(0, i) + remaining.substring(i + 1);
+                generatePermutationsHelper(prefix + remaining.charAt(i), remaining1, permutations);
+            }
+        }
+    }
+}
---
+++ src/main/java/leetcode/thread/PrintNumbers.java
@@ -0,0 +1,56 @@
+package leetcode.thread;
+
+/**
+ * @author Alan
+ * @Description
+ * @date 2024.02.21 20:58
+ */
+public class PrintNumbers {
+    private volatile int number = 0;
+    private final Object lock = new Object();
+
+    public void printEven() {
+        while (number <= 200) {
+            if (number % 2 == 0) {
+                System.out.println(Thread.currentThread().getName() + ": " + number);
+                number++;
+                lock.notify(); // 唤醒等待的线程
+            } else {
+                try {
+                    lock.wait(); // 当前线程等待
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void printOdd() {
+        while (number <= 200) {
+            if (number % 2 != 0) {
+                System.out.println(Thread.currentThread().getName() + ": " + number);
+                number++;
+                lock.notify(); // 唤醒等待的线程
+            } else {
+                try {
+                    lock.wait(); // 当前线程等待
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintNumbers printer = new PrintNumbers();
+
+        Thread thread1 = new Thread(printer::printEven);
+        Thread thread2 = new Thread(printer::printOdd);
+
+        thread1.setName("EvenThread");
+        thread2.setName("OddThread");
+
+        thread1.start();
+        thread2.start();
+    }
+}
