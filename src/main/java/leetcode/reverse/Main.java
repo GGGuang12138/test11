e5b78649afc061e9a801133563dba5251f5cb604
GGGuang12138 <ggguang12138@icloud.com>
2024-04-29 16:02:44 +0800

    OD train

---
+++ src/main/java/leetcode/reverse/Main.java
@@ -0,0 +1,15 @@
+package leetcode.reverse;
+
+import leetcode.ListNode;
+
+/**
+ * @author Alan
+ * @Description
+ * @date 2024.04.24 20:53
+ */
+class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
