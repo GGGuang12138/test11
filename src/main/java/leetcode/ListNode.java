357200dcee5523c06ec12259804b10a23ea5374b
GGGuang12138 <ggguang12138@icloud.com>
2024-05-21 11:30:17 +0800

    node

--- src/main/java/leetcode/ListNode.java
+++ src/main/java/leetcode/ListNode.java
@@ -6,9 +6,9 @@ package leetcode;
  * @date 2024.01.12 22:39
  */
 public class ListNode {
-      int val;
+      public int val;
       public ListNode next;
-      ListNode() {}
-    ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     public ListNode() {}
+     public ListNode(int val) { this.val = val; }
+     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
